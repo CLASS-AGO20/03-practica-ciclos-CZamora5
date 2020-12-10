ea2ae225dea4b4e56f7872a17f0311a1b607e586
Carlos Zamora <czamora5@ucol.mx>
2020-12-10 15:41:04 -0600

    Implemented factorial method

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
-export default class App {}
+export default class App {
+    factorial(numero){
+        let result = 1;
+        for(let i=1; i <= numero; i++){
+            result *= i;
+        }
+        return result;
+    }
+}
 
 let app = new App();
